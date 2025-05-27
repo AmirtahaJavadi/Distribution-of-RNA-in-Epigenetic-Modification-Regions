0ab0a46538e438e93f079ee6ef88b2b0b9f28de6
Amirtaha Javadi <amirtahajavadidev@gmail.com>
2025-05-27 11:55:02 +0330

    functionallty added

--- main.py
+++ main.py
@@ -1,31 +1,36 @@
 from Bio import SeqIO
 
-# Path to your .fna file
-file_path = "gene.fna"
+def count_fivemers(file_path , gene_id):
+    file_path = file_path
+    countfivemers = {}
 
-for record in SeqIO.parse(file_path, "fasta"):
-    print(record.id)
+    for record in SeqIO.parse(file_path, "fasta"):
+        print(record.id)
 
-# Parse the FASTA file
-fivemers = []
-for record in SeqIO.parse(file_path, "fasta"):
-    # Extract the 5-mers from the sequence
-    if record.id == "NC_000006.12:35342558-35428178":
-        for i in range(len(record.seq) - 5):
-            fivemer = record.seq[i:i+5]
-            # Add the 5-mer to the list
-            fivemers.append(fivemer)
+    # Parse the FASTA file
+    fivemers = []
+    for record in SeqIO.parse(file_path, "fasta"):
+        # Extract the 5-mers from the sequence
+        if record.id == gene_id:
+            for i in range(len(record.seq) - 5):
+                fivemer = record.seq[i:i+5]
+                # Add the 5-mer to the list
+                fivemers.append(fivemer)
 
-# Print the list of 5-mers
-print(fivemers)
 
-countfivemers = {}
 
-for fivemer in fivemers:
-    if fivemer in countfivemers:
-        countfivemers[fivemer] += 1
-    else:
-        countfivemers[fivemer] = 1
+    for fivemer in fivemers:
+        if fivemer in countfivemers:
+            countfivemers[fivemer] += 1
+        else:
+            countfivemers[fivemer] = 1
 
-# Print the count of 5-mers
-print(countfivemers)
+    # Print the count of 5-mers
+    primary_fivemers = countfivemers
+    print(countfivemers)
+    for key, value in countfivemers.items():
+        if value <= 1 :
+            primary_fivemers.pop(key)
+            
+
+count_fivemers("gene.fna" ,"NC_000006.12:35342558-35428178")
